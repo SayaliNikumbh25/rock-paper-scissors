689ee891683a7e78e255dc60808ef696b38f3a4c
SayaliNikumbh25 <sayalinikumbh7890@gmail.com>
2024-03-18 00:18:14 +0530

    NaN error

--- app.js
+++ app.js
@@ -13,12 +13,12 @@ let next_button = document.querySelector('#next_btn');
 let winner_container= document.querySelector('.winner_container')
 let play_again_winner_btn =document.querySelector('.play_again_winner');
 
-if (!localStorage.getItem('pc_count')) {
-    localStorage.setItem('pc_count', '0');
-}
-if (!localStorage.getItem('user_count')) {
-    localStorage.setItem('user_count', '0');
-}
+// if (!localStorage.getItem('pc_count')) {
+//     localStorage.setItem('pc_count', '0');
+// }
+// if (!localStorage.getItem('user_count')) {
+//     localStorage.setItem('user_count', '0');
+// }
 
 /*--------------------------------- displaying user score and PC score------------------------------------- */
 
