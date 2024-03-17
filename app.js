8e97629e8c95dbfa4d0777305e91938cd68e6318
SayaliNikumbh25 <sayalinikumbh7890@gmail.com>
2024-03-18 00:04:39 +0530

    NaN error

--- app.js
+++ app.js
@@ -94,9 +94,9 @@ function displayResult(result){
 function winner(choice,pc_choice){
     if(choice.name == pc_choice.beats){
         document.querySelector('.winner_heading').innerHTML = '<h2 class="result_text">you lost</h2><h3>against PC</h3>';
-        localStorage.pc_count= Number(localStorage.pc_count)+1;
-        pcScore.innerHTML = localStorage.pc_count;
-        console.log(Number(localStorage.pc_count)+1);
+        localStorage.pc_count = Number(localStorage.pc_count)+1;
+        console.log(localStorage.pc_count);
+        pcScore.innerHTML= localStorage.pc_count
         result_button[1].classList.add('winner'); 
         result_button[0].classList.remove('winner');
         removeNextButton();
@@ -104,9 +104,9 @@ function winner(choice,pc_choice){
         }
     else if (choice.beats == pc_choice.name){
         document.querySelector('.winner_heading').innerHTML = '<h2 class="result_text">you win</h2><h3>against PC</h3>';
-        localStorage.user_count=Number(localStorage.user_count)+1;
-        userScore.innerHTML = localStorage.user_count;
-        console.log(Number(localStorage.user_count)+1);
+        localStorage.user_count = Number(localStorage.pc_count)+1;
+        console.log(localStorage.user_count);
+        userScore.innerHTML = localStorage.user_count
         result_button[0].classList.add('winner');
         result_button[1].classList.remove('winner'); 
         addNextButton();
